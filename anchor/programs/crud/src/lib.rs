51b277fd80845fddea4d19f305b1437dce6577c1
t9fiction <sohail.sohailishaq@gmail.com>
2025-01-20 03:16:30 +0500

    create entry written in contract

--- anchor/programs/crud/src/lib.rs
+++ anchor/programs/crud/src/lib.rs
@@ -4,67 +4,68 @@ use anchor_lang::prelude::*;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
+// Program constants
+const ANCHOR_DISCRIMINATOR_SPACE: usize = 8;
+const SPACE_PUBKEY: usize = 32;
+const SPACE_STRING: usize = 4 + 50;
+const SPACE_MESSAGE: usize = 4 + 280;
+
 #[program]
 pub mod crud {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseCrud>) -> Result<()> {
-    Ok(())
-  }
+    pub fn create_journal_entry(ctx: Context<CreateJournalEntry>, title: String, message: String) -> Result<()>{
+      let journal_entry = &mut ctx.accounts.journal_entry;
+      journal_entry.owner = ctx.accounts.owner.key();
+      journal_entry.title = title;
+      journal_entry.message = message;
+      Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crud.count = ctx.accounts.crud.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.crud.count = ctx.accounts.crud.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCrud>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.crud.count = value.clone();
-    Ok(())
-  }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCrud<'info> {
+#[instruction(title: String. message: String)]
+pub struct CreateJournalEntry<'info>{
   #[account(mut)]
-  pub payer: Signer<'info>,
+  pub owner: Signer<'info>,
 
   #[account(
-  init,
-  space = 8 + Crud::INIT_SPACE,
-  payer = payer
+    init,
+    payer= owner,
+    space= ANCHOR_DISCRIMINATOR_SPACE + JournalEntryState::INIT_SPACE,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    constraint = title.len() <= 50 @ CrudError::TitleTooLong,
+    constraint = message.len() <= 280 @ CrudError::MessageTooLong,
+    constraint = title.len() > 0 @ CrudError::TitleTooShort,
+    constraint = message.len() > 0 @ CrudError::MessageTooShort,
   )]
-  pub crud: Account<'info, Crud>,
+  pub journal_entry: Account<'info, JournalEntryState>,
+
   pub system_program: Program<'info, System>,
 }
-#[derive(Accounts)]
-pub struct CloseCrud<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub crud: Account<'info, Crud>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub crud: Account<'info, Crud>,
-}
 
 #[account]
 #[derive(InitSpace)]
-pub struct Crud {
-  count: u8,
+pub struct JournalEntryState{
+  pub owner: Pubkey,
+  #[max_len(50)]
+  pub title: String,
+  #[max_len(280)]
+  pub message: String,
+}
+
+
+#[error_code]
+pub enum CrudError {
+  #[msg("The provided title should be 50 characters long maximum.")]
+  TitleTooLong,
+  #[msg("The provided message should be 280 characters long maximum.")]
+  MessageTooLong,
+  #[msg("The provided title should be 1 character long minimum.")]
+  TitleTooShort,
+  #[msg("The provided message should be 1 character long minimum.")]
+  MessageTooShort,
 }
