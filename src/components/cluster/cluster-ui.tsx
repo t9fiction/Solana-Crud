ed546d37f637b888ad37088d4b7b0f9613d9a8de
t9fiction <sohail.sohailishaq@gmail.com>
2025-01-20 23:14:58 +0500

    crud property removed from the cluster ui also

--- src/components/cluster/cluster-ui.tsx
+++ src/components/cluster/cluster-ui.tsx
@@ -100,14 +100,12 @@ export function ClusterUiModal({ hideModal, show }: { hideModal: () => void; sho
     >
       <input
         type="text"
-        crud="Name"
         className="input input-bordered w-full"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <input
         type="text"
-        crud="Endpoint"
         className="input input-bordered w-full"
         value={endpoint}
         onChange={(e) => setEndpoint(e.target.value)}
